e0c347698dadc37d02ca37b735b77427fe217b8c
Syed25794 <syedgulmohammad25794@gmail.com>
2022-12-29 20:43:48 +0530

    added cors package

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const connection = require("./config/database");
 
 const messageRouter = require("./routes/message.routes");
 
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
